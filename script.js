d23de91522c2cf5736cf0a7cac002cc4dc7a54cf
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-04-04 14:39:25 -0700

    Adding Functionality for Edge Cases
    - Missing edges case functionality. For example, multiple calculations in 1 session.
    - Commented out equal button function for now until operator functions are complete.

--- script.js
+++ script.js
@@ -5,50 +5,65 @@ let result = "";
 let tempNumberContainer = "";
 let currentDisplayScreen = "";
 const displayScreen = document.querySelector("#calcScreen");
+const updateDisplayScreen = () => displayScreen.textContent = currentDisplayScreen;
 
-// Event Listener for all non-operator buttons
+// Number Button Functionality
 document.querySelectorAll(".displayButton").forEach(button => {
         button.addEventListener("click", () => {
-            if (result != "") {
-                currentDisplayScreen = "";
-                displayScreen.textContent = currentDisplayScreen;
+            if (result === "") { // 1st Calculation
                 currentDisplayScreen += button.textContent;
                 tempNumberContainer += button.textContent;
-                displayScreen.textContent = currentDisplayScreen;
-            } else if (result === "") {
-                currentDisplayScreen += button.textContent;
-                tempNumberContainer += button.textContent;
-                displayScreen.textContent = currentDisplayScreen;
-            };
+                updateDisplayScreen();
+                console.log(`tempNum is ${tempNumberContainer} | num1 is ${num1} | operation is ${operator} | num2 is ${num2} | result is ${result}`);
+            } else if (result !== "") { // Operator was pressed instead of equal e.g. (2 + 3 + )
+                num1 = result;
+                result = "";
+                num2 = button.textContent;
+                currentDisplayScreen = `${num1} ${operator} ${num2}`;
+                updateDisplayScreen();
+                console.log(`tempNum is ${tempNumberContainer} | num1 is ${num1} | operation is ${operator} | num2 is ${num2} | result is ${result}`);
+             };
         });
     });
-
-// Event Listener for all operator buttons
+            
+// Operator Button Functionality (+, -, *, /)
 document.querySelectorAll(".operator").forEach(button => {
     button.addEventListener("click", () => {
-        num1 = tempNumberContainer; // Move user input from temp variable to num1  
-        tempNumberContainer = ""; // Clear temp variable
-        operator = button.textContent; // add operator to variable
-        currentDisplayScreen = num1 + " " + operator + " " + num2; // add operator to display screen variable
-        displayScreen.textContent = currentDisplayScreen; // update display screen
-    })
-})
+        if (result === "" && num1 === "") { // Operator pressed for 1st time
+            num1 = tempNumberContainer;
+            tempNumberContainer = "";
+            operator = button.textContent;
+            currentDisplayScreen = `${num1} ${operator} ${num2}`;
+            // num1 = "";
+            updateDisplayScreen();
+            console.log(`tempNum is ${tempNumberContainer} | num1 is ${num1} | operation is ${operator} | num2 is ${num2} | result is ${result}`);
+        } else if (result === "" && num1 != "") { // Operator is pressed instead of equals
+            num2 = tempNumberContainer;
+            tempNumberContainer = "";
+            result = operate(num1, operator, num2);
+            currentDisplayScreen = `${result} ${operator}`;
+            updateDisplayScreen();
+            operator = button.textContent;
+            console.log(`tempNum is ${tempNumberContainer} | num1 is ${num1} | operation is ${operator} | num2 is ${num2} | result is ${result}`);
+        } else if (result === "" && num1 !== "" && num2 !== "") {
+            
+        }
+    });
+});
 
-// Event Listener for Equal button
-document.querySelector("#equals").addEventListener("click", () => {
-    num2 = tempNumberContainer; // Move user input from temp variable to num2
-    tempNumberContainer = ""; // Clear temp variable
-    if (result === "") {
-        result = operate(num1, operator, num2);
-    } else {
-        result = operate(result, operator, num2);
-    };
-    displayScreen.textContent = result;
-    num1 = "";
-    num2 = "";
-    operator = "";
-    tempNumberContainer = "";
-})
+// // Event Listener for Equal button
+// document.querySelector("#equals").addEventListener("click", () => {
+//     num2 = tempNumberContainer; // Move user input from temp variable to num2
+//     tempNumberContainer = ""; // Clear temp variable
+//     if (result === "") {
+//         result = operate(num1, operator, num2);
+//         console.log(`tempNum is ${tempNumberContainer} | num1 is ${num1} | operation is ${operator} | num2 is ${num2} | result is ${result}`);
+//     } else {
+//         result = operate(result, operator, num2);
+//         console.log(`tempNum is ${tempNumberContainer} | num1 is ${num1} | operation is ${operator} | num2 is ${num2} | result is ${result}`);
+//     };
+//     displayScreen.textContent = result;
+// })
 
 // Clear Button
 document.querySelector("#clear").addEventListener("click", () => {
@@ -58,7 +73,8 @@ document.querySelector("#clear").addEventListener("click", () => {
     tempNumberContainer = "";
     currentDisplayScreen = "";
     result = "";
-    displayScreen.textContent = currentDisplayScreen;
+    updateDisplayScreen();
+    console.log(`tempNum is ${tempNumberContainer} | num1 is ${num1} | operation is ${operator} | num2 is ${num2} | result is ${result}`);
 });
 
 // Individual Calculation Functions
@@ -91,10 +107,10 @@ function operate(num1, operator, num2) {
     };
 };
 
-module.exports = {
-    add,
-    subtract,
-    multiply,
-    divide,
-    operate,
-};
+// module.exports = {
+//     add,
+//     subtract,
+//     multiply,
+//     divide,
+//     operate,
+// };
